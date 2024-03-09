ab1b9ef922cbc08b24348ee25d353163c38253ad
Brian <brdiep113@gmail.com>
2024-03-08 23:18:17 -0800

    change gpus

--- calibrate_test.sh
+++ calibrate_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --mem=64G
 #SBATCH --nodes=1
-#SBATCH --gpus=per-node=p100:4
+#SBATCH --gpus-per-node=p100:4
 #SBATCH --ntasks-per-node=4
 #SBATCH --time=1:00:00
 #SBATCH --mail-user=<brdiep@mail.ubc.ca>
