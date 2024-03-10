264f8b062dba9074ac3e6e15e16f08172d6f4292
Brian <brdiep113@gmail.com>
2024-03-09 16:24:30 -0800

    Add cpu only version of script for testing

---
+++ cpu_only_calibrate_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=1:00:00
+#SBATCH --mail-user=<brdiep@mail.ubc.ca>
+#SBATCH --mail-type=ALL
+
+cd ~/$project/whisper-test
+
+# Set up modules/virtual environment
+module purge
+module load StdEnv/2023 python/3.11 scipy-stack gcc/12.3 arrow/14 cuda
+source ~/std23/bin/activate
+
+# Login to W&B to log metrics
+wandb login $API_KEY
+
+python calibrate_test.py --projectname test_calibration --token $HUGGINGFACE_TOKEN --beams 10 --sentences 10
