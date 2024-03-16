5d4a211addbcf1eee7b1fd2c7bc27616038bbd42
Brian <brdiep113@gmail.com>
2024-03-16 15:23:34 -0700

    narval test

--- narval_calibrate_test.py
+++ narval_calibrate_test.py
@@ -7,7 +7,7 @@ from datasets import load_dataset, Audio, DatasetDict, load_from_disk
 import evaluate
 from transformers import WhisperFeatureExtractor, WhisperTokenizer, WhisperProcessor, WhisperForConditionalGeneration
 from whisper_utils import DataCollatorSpeechSeq2SeqWithPadding
-import wandb
+import webdataset as wds
 from calibrate import calibrate, conformal_test
 
 
@@ -36,36 +36,23 @@ if __name__ == "__main__":
     tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-small", local_files_only=True, language="en", task="transcribe")
     processor = WhisperProcessor.from_pretrained("openai/whisper-small", local_files_only=True, language="en", task="transcribe")
 
-    def prepare_dataset(batch):
-        # load and resample audio data from 48 to 16kHz
-        audio = batch["audio"]
-        
-        # compute log-Mel input features from input audio array 
-        batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
-        
-        # encode target text to label ids 
-        batch["labels"] = tokenizer(batch["sentence"]).input_ids
-        
-        return batch
+    # Get data loaders
+    valid_set = (
+        wds.WebDataset("~/scratch/processed_common_voice/en--en_dev_0.tar")
+        .decode()
+        .shuffle(size=1000)
+        .batch(batchsize=10)
+    )
 
-    token = args.token
-    cv = DatasetDict()
-    cv["validation"] = load_dataset("mozilla-foundation/common_voice_13_0", "en", split="validation[:1%]", 
-                                    token=token, trust_remote_code=True, 
-                                    cache_dir="~/scratch/brdiep/.cache/huggingface/datasets")
-    cv["test"] = load_dataset("mozilla-foundation/common_voice_13_0", "en", split="test[:1%]",
-                              token=token, trust_remote_code=True, 
-                              cache_dir="~/scratch/brdiep/.cache/huggingface/datasets")
-    cv = cv.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
-    cv = cv.cast_column("audio", Audio(sampling_rate=16000))
-    cv = cv.map(prepare_dataset, remove_columns=cv.column_names["train"], num_proc=4)
-    # Create data loaders
-    # data = load_from_disk("~/scratch/brdiep/cv_en")
-    calibDataLoader = construct_dataloader(cv, "validation") 
-    evalDataLoader = construct_dataloader(cv, "test")
+    test_set = (
+        wds.WebDataset("~/scratch/processed_common_voice/en--en_test_0.tar")
+        .decode()
+        .shuffle(size=1000)
+        .batch(batchsize=10)
+    )
 
     # Load models
-    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small", local_files_only=True)
     model.config.forced_decoder_ids = None
     model.config.suppress_tokens = []
     model.to(device)
@@ -73,7 +60,7 @@ if __name__ == "__main__":
     # Calibrate on calibration set
     lhat = calibrate(model=model,
                      processor=processor,
-                     data_loader=calibDataLoader,
+                     data_loader=valid_set,
                      wer_target=0.2,
                      epsilon=0.0001,
                      alpha=0.2,
@@ -85,7 +72,7 @@ if __name__ == "__main__":
     # Evaluate empirical coverage alpha_hat and mean conformal prediction set size
     alpha_hat, mean_conformal_set = conformal_test(model=model,
                                                    processor=processor,
-                                                   data_loader=evalDataLoader,
+                                                   data_loader=test_set,
                                                    wer_target=0.2,
                                                    num_beams=args.beams,
                                                    max_sentences=args.sentences
