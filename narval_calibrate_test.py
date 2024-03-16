6d96e1808f2a0309fc6e35a939a0348d14cff70a
Brian <brdiep113@gmail.com>
2024-03-16 15:07:47 -0700

    Add internet-less script for narval

---
+++ narval_calibrate_test.py
@@ -0,0 +1,96 @@
+import argparse
+import os
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import BatchSampler, RandomSampler
+from datasets import load_dataset, Audio, DatasetDict, load_from_disk
+import evaluate
+from transformers import WhisperFeatureExtractor, WhisperTokenizer, WhisperProcessor, WhisperForConditionalGeneration
+from whisper_utils import DataCollatorSpeechSeq2SeqWithPadding
+import wandb
+from calibrate import calibrate, conformal_test
+
+
+def construct_dataloader(dataset, set, batch_size=32, drop_last=False):
+    data = dataset[set]
+    batch_sampler = BatchSampler(RandomSampler(data[set]), batch_size=batch_size, drop_last=drop_last)
+    dataloader = DataLoader(data, batch_sampler=batch_sampler)
+
+    return dataloader
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Whisper Tutorial",
+        description="Sample tutorial for running Whisper on Cedar and logging metrics with wandb"
+    )
+    parser.add_argument("-t", "--token", help="User Access Token")
+    parser.add_argument("-b", "--beams", metavar='N', type=int, nargs='+', help="Number of beams")
+    parser.add_argument("-s", "--sentences", metavar='N', type=int, nargs='+', help="Number of sentences")
+
+    args = parser.parse_args()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    # Load data
+    feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-small", local_files_only=True)
+    tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-small", local_files_only=True, language="en", task="transcribe")
+    processor = WhisperProcessor.from_pretrained("openai/whisper-small", local_files_only=True, language="en", task="transcribe")
+
+    def prepare_dataset(batch):
+        # load and resample audio data from 48 to 16kHz
+        audio = batch["audio"]
+        
+        # compute log-Mel input features from input audio array 
+        batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
+        
+        # encode target text to label ids 
+        batch["labels"] = tokenizer(batch["sentence"]).input_ids
+        
+        return batch
+
+    token = args.token
+    cv = DatasetDict()
+    cv["validation"] = load_dataset("mozilla-foundation/common_voice_13_0", "en", split="validation[:1%]", 
+                                    token=token, trust_remote_code=True, 
+                                    cache_dir="~/scratch/brdiep/.cache/huggingface/datasets")
+    cv["test"] = load_dataset("mozilla-foundation/common_voice_13_0", "en", split="test[:1%]",
+                              token=token, trust_remote_code=True, 
+                              cache_dir="~/scratch/brdiep/.cache/huggingface/datasets")
+    cv = cv.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
+    cv = cv.cast_column("audio", Audio(sampling_rate=16000))
+    cv = cv.map(prepare_dataset, remove_columns=cv.column_names["train"], num_proc=4)
+    # Create data loaders
+    # data = load_from_disk("~/scratch/brdiep/cv_en")
+    calibDataLoader = construct_dataloader(cv, "validation") 
+    evalDataLoader = construct_dataloader(cv, "test")
+
+    # Load models
+    model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
+    model.config.forced_decoder_ids = None
+    model.config.suppress_tokens = []
+    model.to(device)
+
+    # Calibrate on calibration set
+    lhat = calibrate(model=model,
+                     processor=processor,
+                     data_loader=calibDataLoader,
+                     wer_target=0.2,
+                     epsilon=0.0001,
+                     alpha=0.2,
+                     delta=0.1,
+                     num_beams=args.beams,
+                     max_sentences=args.sentences
+                     )
+    
+    # Evaluate empirical coverage alpha_hat and mean conformal prediction set size
+    alpha_hat, mean_conformal_set = conformal_test(model=model,
+                                                   processor=processor,
+                                                   data_loader=evalDataLoader,
+                                                   wer_target=0.2,
+                                                   num_beams=args.beams,
+                                                   max_sentences=args.sentences
+                                                   )
+    
+    print(lhat)
+    print(alpha_hat)
+    print(mean_conformal_set)
