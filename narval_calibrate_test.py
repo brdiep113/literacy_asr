2a0ebe9950403a1af6d3b2009c4ac2aa9fb857b1
Brian <brdiep113@gmail.com>
2024-03-18 22:22:47 -0700

    Add lhat

--- narval_calibrate_test.py
+++ narval_calibrate_test.py
@@ -72,6 +72,7 @@ if __name__ == "__main__":
     alpha_hat, mean_conformal_set = conformal_test(model=model,
                                                    processor=processor,
                                                    test_loader=test_set,
+                                                   lhat=lhat,
                                                    wer_target=0.2,
                                                    num_beams=args.beams,
                                                    max_sentences=args.sentences
